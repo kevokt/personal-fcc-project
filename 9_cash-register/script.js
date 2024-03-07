dafd075d6d733f38fe914b576d74dac322961ba6
kevokt <kevinoktavian08@gmail.com>
2024-03-07 17:08:55 +0700

    add a bit js code and changes html and css a bit

--- 9_cash-register/script.js
+++ 9_cash-register/script.js
@@ -10,3 +10,31 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100]
 ];
+
+const changeDue = document.getElementById("change-due");
+const cashInput = document.getElementById("cash");
+const btn = document.getElementById("purchase-btn");
+const priceDisplay = document.querySelector(".price");
+const cidList = document.querySelectorAll("span");
+
+// For displaying price in cash Register
+priceDisplay.textContent = `Price: $${price}`;
+
+// For displaying cid in cash register
+cidList.forEach((price, index) => {
+  price.textContent = `$${cid[index][1]}`;
+});
+
+btn.addEventListener("click", () => {
+  // If customer pays less the price amount
+  if (Number(cashInput.value) < price) {
+    alert("Customer does not have enough money to purchase the item");
+    return;
+  }
+
+  // If customer pays with the exact amount of price 
+  if (Number(cashInput.value) === price) {
+    changeDue.innerHTML = `<p>No change due - customer paid with exact cash</p>`;
+    return;
+  }
+});
