233442e2c94d3b4eba2a61df52e8b2c84e83e2b8
kevokt <kevinoktavian08@gmail.com>
2024-03-08 15:01:04 +0700

    change cid and price

--- 9_cash-register/script.js
+++ 9_cash-register/script.js
@@ -1,15 +1,5 @@
-let price = 1.87;
-let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-];
+let price = 19.5;
+let cid = [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
 
 const changeDue = document.getElementById("change-due");
 const cashInput = document.getElementById("cash");
